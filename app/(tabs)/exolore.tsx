b41e13a35edbc99cf926d0d24d92fc7824a40aef
Wambuarack <shedrackwambua40@gmail.com>
2025-10-28 03:27:32 +0300

    vv

---
+++ app/(tabs)/exolore.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function exolore() {
+  return (
+    <View>
+      <Text>exolore</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
