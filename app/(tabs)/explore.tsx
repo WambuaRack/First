ece043995499b798a8b87c3568e26866dedc97bf
Wambuarack <shedrackwambua40@gmail.com>
2025-10-28 04:08:49 +0300

    style

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -1,12 +1,24 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
 
-export default function exolore() {
+export default function Explore() {
   return (
-    <View>
-      <Text>exolore</Text>
+    <View style={styles.container}>
+      <Text style={styles.text}> Explore Tab</Text>
     </View>
-  )
+  );
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#f4f4f4',
+  },
+  text: {
+    fontSize: 20,
+    fontWeight: '600',
+    color: '#007AFF',
+  },
+});
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,25 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
 
-export default function index() {
+export default function Index() {
   return (
-    <View>
-      <Text>index</Text>
+    <View style={styles.container}>
+      <Text style={styles.text}> Home Tab</Text>
     </View>
-  )
+  );
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 60,
+    backgroundColor: '#f9f9f9', // light background
+  },
+  text: {
+    fontSize: 20,
+    fontWeight: '600',
+    color: '#333',
+  },
+});
