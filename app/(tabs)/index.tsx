05bd4d531e350e03ec9875111d1a3ce7669a8c49
Wambuarack <shedrackwambua40@gmail.com>
2025-10-28 12:15:26 +0300

    xcss

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -5,9 +5,9 @@ const logoimg =require("../../assets/images/android-icon-background.png");
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}> Home Tab</Text>
-      <Image source={logoimg}/>
-
+      <Text style={styles.text}> Home </Text>
+      <View style={styles.container}><Text style={styles.text}> Are you currently a  member? </Text></View>
+    
     <View style={styles.container}>
       <Button
         title="Click me"
@@ -25,16 +25,18 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     padding: 60,
-    backgroundColor: '#f9f9f9', // light background
+    backgroundColor: '#292c37ff', // light background
   },
   text: {
-    fontSize: 20,
+    fontSize: 15,
     fontWeight: '600',
-    color: '#333',
+    color: '#ffffffff',
+    gap:20
+
   },
   image:{
-    height:150,
-    width:150,
-    margin:40
+    height:100,
+    width:100,
+    margin:10
   }
 });
