beb0579a62e7b5d90517b7d69a458ddd495717c4
Wambuarack <shedrackwambua40@gmail.com>
2025-10-28 11:09:14 +0300

    imge

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,12 @@
-import { StyleSheet, Text, View } from 'react-native';
-import React from 'react';
+import { StyleSheet, Text,Image, View } from 'react-native';
 
+import React from 'react';
+const logoimg =require("../../assets/images/android-icon-background.png");
 export default function Index() {
   return (
     <View style={styles.container}>
       <Text style={styles.text}> Home Tab</Text>
+      <Image source={logoimg}/>
     </View>
   );
 }
